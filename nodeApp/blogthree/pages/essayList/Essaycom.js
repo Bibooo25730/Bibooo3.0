51fddacaf437ebc8792c607d37262d3d2d891ae2
Bibooo25730 <2573018577@qq.com>
2023-04-23 02:21:41 +0800

    acction

---
+++ nodeApp/blogthree/pages/essayList/Essaycom.js
@@ -0,0 +1,14 @@
+import Header from "../header/Header"
+import essaycom from "./Essaycom.module.css"
+
+
+export default function Essaycom(){
+    return (
+        <div>
+        <Header></Header>
+        <div id="Essaycom" className={essaycom.section}>12312</div>
+    </div>
+    )
+
+}
+
---
+++ nodeApp/blogthree/pages/install.js
@@ -0,0 +1,12 @@
+window.addEventListener('install', function (event) {
+    console.log(event)
+    event.waitUntil(
+      caches.open('http://localhost:3000/').then(function (cache) {
+        return cache.addAll([
+          '/styles/globals.css',
+          '/styles/zpix.css',
+          // etc.
+        ]);
+      }),
+    );
+  });
