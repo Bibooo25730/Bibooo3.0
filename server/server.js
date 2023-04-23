4d77dfcc62f92d20ed688bc229cf13e1ab0774b9
Bibooo25730 <2573018577@qq.com>
2023-04-23 14:38:12 +0800

    server

--- server/server.js
+++ server/server.js
@@ -15,10 +15,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors()); //注入 cors 模块解决跨域
 // 测试
 app.get("/",(req,res)=>{
-	res.send("Hi Hello word");
+	res.send("Hi Hello Bibooo");
 })
 app.use("/",routes);
 app.listen(PORT, () => {
 	
-	console.log(`server started at localhost:${PORT}}`)
+	console.log(`server started at localhost:${PORT}`)
 });
