da17a705733c36a7cf1dfcdf21c374f693b73728
Bibooo25730 <2573018577@qq.com>
2023-05-05 23:57:46 +0800

    zoom

--- router/play.js
+++ router/play.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const { createServer }  = require('http');
+const httpsever = createServer();
+const { Server } = require("socket.io")
+const io = new Server(httpsever, {
+  cors:true
+});
+
 const Router = express.Router();
 const play = require("../server/playdesc");
 Router.get("/Stardesc",play.Stardesc);
+io.on("connection", (socket) => {
+  socket.on('chat', (msg) => {
+    socket.broadcast.emit('servermsg',msg);
+  });
+  });
+httpsever.listen(3000,()=>{
+    console.log('listening on *:3000');
+})
 
 module.exports = Router
